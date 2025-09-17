2acf16d17beb0df4f2c62c0242c076cf3fdee03d
MgKyaw <mgkyaw20112@gmail.com>
2025-09-17 21:07:33 +0700

    feat: Update AccountNumberValidationTests

--- StartingProject/EmployeesApp/EmployeesApp.Tests/Validation/AccountNumberValidationTests.cs
+++ StartingProject/EmployeesApp/EmployeesApp.Tests/Validation/AccountNumberValidationTests.cs
@@ -1,10 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace EmployeesApp.Tests.Validation;
 
-namespace EmployeesApp.Tests.Validation;
-internal class AccountNumberValidationTests
+public class AccountNumberValidationTests
 {
+    private readonly AccountNumberValidation _validation;
+
+    public AccountNumberValidationTests() => _validation = new AccountNumberValidation();
+
+    [Fact]
+    public void IsValid_ValidAccountNumber_ReturnsTrue()
+        => Assert.True(_validation.IsValid("123-4543234576-23"));
 }
