4ed515a16b7bf9691b49fb809487a3b4ecb747a7
MgKyaw <mgkyaw20112@gmail.com>
2025-09-18 21:29:08 +0700

    feat: Add more tests

--- StartingProject/EmployeesApp/EmployeesApp.Tests/Validation/AccountNumberValidationTests.cs
+++ StartingProject/EmployeesApp/EmployeesApp.Tests/Validation/AccountNumberValidationTests.cs
@@ -17,4 +17,16 @@ public class AccountNumberValidationTests
     [InlineData("12-3454565676-23")]
     public void IsValid_AccountNumberFirstPartWrong_ReturnsFalse(string accountNumber)
     => Assert.False(_validation.IsValid(accountNumber));
+
+    [Theory]
+    [InlineData("123-345456567-23")]
+    [InlineData("123-345456567633-23")]
+    public void IsValid_AccountNumberMiddlePartWrong_ReturnsFalse(string accNumber)
+    => Assert.False(_validation.IsValid(accNumber));
+
+    [Theory]
+    [InlineData("123-3434545656-2")]
+    [InlineData("123-3454565676-233")]
+    public void IsValid_AccountNumberLastPartWrong_ReturnsFalse(string accNumber)
+        => Assert.False(_validation.IsValid(accNumber));
 }
